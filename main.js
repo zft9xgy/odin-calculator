c5b02452d599386dd962543bc49862db907e04bd
zft9xgy <zft9xgy@icloud.com>
2023-07-18 15:40:50 +0200

    Adding operate function

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+var firstOperand = 0;
+var secondOperand = 0;
+var operator = "add";
+
+function operate(firstOperand, secondOperand, operator) {
+	switch (operator) {
+		case "add":
+			return add(firstOperand, secondOperand);
+			break;
+		case "substract":
+			return substract(firstOperand, secondOperand);
+			break;
+		case "multiply":
+			return multiply(firstOperand, secondOperand);
+			break;
+		case "divide":
+			return divide(firstOperand, secondOperand);
+			break;
+		default:
+			break;
+	}
+}
